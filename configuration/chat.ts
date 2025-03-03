3af9e09329066ceb7b33cb4fb4ef188bbc5a8608
Giovanna Chen <111817508+chio2069@users.noreply.github.com>
2025-03-03 13:23:09 -0500

    Update chat.ts
    more changes to initial message

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant! How can I help you hit your fitness goals today?`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}! How can I help you hit your fitness goals today?`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Whew! That was a tough question! I’m catching my breath but I’ll be back to help you crush your goals!`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `Whoa, that's a heavy lift! Try trimming it down a bit so I can handle it—let's keep it lean and strong!`;
